7e1432a9815325754893e9d558ed34cfb48d9e54
pan <66984022+pan212@users.noreply.github.com>
2025-09-18 06:49:23 +0800

    更新 sht.js

--- sht.js
+++ sht.js
@@ -1,15 +1,12 @@
 let body = $response.body;
 
-// 删除广告块：class="n5_tbys cl"
+// 删除顶部广告 logo 区块
 body = body.replace(/<div class="n5_tbys cl">[\s\S]*?<\/div>/g, "");
 
-// 删除广告文字列表：class="show-text cl"
+// 删除广告文字列表
 body = body.replace(/<div class="show-text cl">[\s\S]*?<\/div>/g, "");
 
-// 删除广告轮播：class="n5_jujiao"
+// 删除广告轮播
 body = body.replace(/<div class="n5_jujiao">[\s\S]*?<\/div>/g, "");
-
-// 删除隐藏的冗余 h10 div
-body = body.replace(/<div class="h10"[^>]*><\/div>/g, "");
 
 $done({ body });
